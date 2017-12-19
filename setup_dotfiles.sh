b92ff9d9e65b62a1d10d3f514e69891e64f34fb1
Ian Howell <ian.howell0@gmail.com>
2017-12-19 12:10:46 -0600

    Move setup.sh to setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 ############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
 ############################
 
 ########## Variables
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="profile bashrc sh_aliases bash_logout vimrc vim inputrc zshrc"    # list of files/folders to symlink in homedir
+files="profile bashrc sh_aliases vim inputrc"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
 cd $dir
-echo "done"
-
-# Create the config directory
-echo -n "Creating the config directory ..."
-mkdir -p config
-echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 echo "Moving any existing dotfiles from ~ to $olddir"
@@ -38,4 +30,5 @@ done
 # Create the vim directories
 echo -n "Creating vim backup directory ..."
 mkdir -p vim/undo
+
 echo "done"
