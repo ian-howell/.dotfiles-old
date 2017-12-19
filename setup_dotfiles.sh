e56a377a057d5b7e121eb89d841cb2b6b808d934
Ian Howell <ian.howell0@gmail.com>
2017-12-19 22:43:28 +0000

    Fix vim file structure

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -7,24 +7,30 @@
 
 dir=$HOME/.dotfiles                    # dotfiles directory
 olddir=$HOME/.dotfiles_old             # old dotfiles backup directory
-files="profile bashrc sh_aliases vim vimrc inputrc"    # list of files/folders to symlink in homedir
-
+files="profile bashrc sh_aliases vim vimrc inputrc gitconfig"    # list of files/folders to symlink in homedir
 ##########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in $HOME ..."
 if [ ! -e $olddir ]; then
-    mkdir -p $olddir
+   mkdir -p $olddir
 fi
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory ..."
 cd $dir
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the $HOME/dotfiles directory specified in $files
+# Delete oldfiles, move any existing dotfiles in homedir to dotfiles_old
+# directory, then create symlinks from the homedir to any files in the
+# $HOME/dotfiles directory specified in $files
+if [ -d $olddir ]; then
+    rm -rf $olddir
+fi
 echo "Moving any existing dotfiles from $HOME to $olddir"
 for file in $files; do
-    if [ -e $HOME/.$file ]; then
+    if [ -h $HOME/.$file ]; then
+        rm $HOME/.$file
+    elif [ -e $HOME/.$file ]; then
         mv -f $HOME/.$file $olddir
     fi
     echo "Creating symlink to $file in home directory."
