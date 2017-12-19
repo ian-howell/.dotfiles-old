d6a247a2fb6aa668c387c7dfe723d8f22887273b
Ian Howell <ian.howell0@gmail.com>
2017-12-19 14:27:05 -0600

    Remove quiet flag from apt-get

--- setup.sh
+++ setup.sh
@@ -20,8 +20,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "Setting up essentials"
-apt-get -q=2 update
-sudo apt install -q=2 libncurses5-dev libgnome2-dev libgnomeui-dev \
+apt-get -y update
+apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
     python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git curl make
